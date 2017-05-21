645f139519959fb43907d791453d3997b1c2bead
Robert Cooper <robert.cooper.qu@gmail.com>
2017-05-21 14:51:29 -0400

    Got the video to work while muted

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-//Mute youtube background video
-var myVideo =  iframe.getElementById('youtube_embed');
-myVideo.mute();
+
+ // Load the IFrame Player API code asynchronously.
+ var tag = document.createElement('script');
+
+ tag.src = "https://www.youtube.com/iframe_api";
+ var firstScriptTag = document.getElementsByTagName('script')[0];
+ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+ // This function creates an <iframe> (and YouTube player)
+ // after the API code downloads.
+ var player;
+ function onYouTubeIframeAPIReady() {
+   player = new YT.Player('youtube_embed', {
+     height: '390',
+     width: '640',
+     videoId: 'gbrkMDkHRqc',
+     events: {
+       'onReady': onPlayerReady
+     }
+   });
+ }
+
+ // The API will call this function when the video player is ready.
+ function onPlayerReady(event) {
+   event.target.playVideo();
+   event.target.mute()
+ }
